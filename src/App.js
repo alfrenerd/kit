45fdace3d5d6a0d6b0e1aa2128bb77e71514686f
Michele Paladino <alfrenerd@gmail.com>
2018-12-21 11:37:07 +0100

    committed to branch everything

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import StockPage from './components/StockPage';
+import Trending from './components/Trending';
+
+import Landing from './components/Landing';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div>
+          <Navbar />
+          <Switch>
+            {/* <Route path='/' component={Home} exact/> */}
+            <Route path='/' component={Landing} exact/>
+            <Route path='/trending' component={Trending}/>
+            {/* <Route path='/:id' component={StockPage}/> */}
+            <Route path='/:id' render={props => <StockPage key={Date.now()} {...props} />}/>
+          </Switch>
+        </div>
+      </Router>
+
+
     );
   }
 }
